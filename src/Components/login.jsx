528058209ceb8b56907e9408a691c00ebeaa4e57
sshivanshg <145559709+sshivanshg@users.noreply.github.com>
2024-05-22 02:01:51 +0530

    update 5

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const LoginPage = (props)=>{
+const LoginPage = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [appType, setAppType] = useState("ott");
   const [errorMessage, setErrorMessage] = useState("");
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,11 +36,11 @@ const LoginPage = (props)=>{
       );
       const data = await response.json();
       console.log(data.status);
-      if (data.status == "success"){
-        navigate("/home")
-        props.setLoginStatus(true)
-      } else{
-        setErrorMessage("Wrong Credentials!")
+      if (data.status === "success") {
+        navigate("/home");
+        props.setLoginStatus(true);
+      } else {
+        setErrorMessage("Wrong Credentials!");
         setTimeout(() => {
           setErrorMessage("");
         }, 5000);
@@ -52,32 +51,37 @@ const LoginPage = (props)=>{
   };
 
   return (
-    <div className=" bg-[#0D0e10] flex justify-center items-center min-h-screen ">
-      <div className="bg-[#0D0e10] p-8 rounded-lg w-full max-w-md">
-        <h2 className="ttext-white text-2xl font-semibold mb-6 text-center text-white">Sign in to continue</h2>
+    <div className="bg-black flex justify-center items-center min-h-screen">
+      <div className="p-8 rounded-lg w-full max-w-md">
+        <h2 className="text-white text-2xl font-semibold mb-6 text-center">
+          Sign in to continue
+        </h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
-            className="w-full px-2 py-2 rounded-md text-sm border-b border-gray-600 bg-[#0D0e10] text-white placeholder-gray-500 focus:outline-none focus:border-white"
+            className="w-full px-2 py-2 rounded-md text-sm border-b border-gray-600 bg-black text-white placeholder-gray-500 focus:outline-none focus:border-white"
           />
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
-            className="w-full px-2 py-2 rounded-md text-sm border-b border-gray-600 bg-[#0D0e10] text-white placeholder-gray-500 focus:outline-none focus:border-white"
+            className="w-full px-2 py-2 rounded-md text-sm border-b border-gray-600 bg-black text-white placeholder-gray-500 focus:outline-none focus:border-white"
           />
-          <button onClick={handleSubmit} className="w-full py-2 bg-purple-700 text-white  rounded-md hover:bg-purple-800 focus:outline-none">
+          <button
+            onClick={handleSubmit}
+            className="w-full py-2 bg-purple-700 text-white rounded-md hover:bg-purple-800 focus:outline-none"
+          >
             Continue
           </button>
           {errorMessage && (
             <div className="text-red-600 text-center mt-2">{errorMessage}</div>
           )}
         </form>
-        <div className="   text-12px text-white leading-tight sm:text-13px 2xl:text-sm mui-style-1okmvh8-termsText text-right">
+        <div className="text-xs text-white text-right mt-4">
           New to JioCinema?{" "}
           <Link to="/home/signup" className="text-blue-500 underline">
             Sign Up here.
@@ -87,4 +91,5 @@ const LoginPage = (props)=>{
     </div>
   );
 };
+
 export default LoginPage;
--- src/Components/signUp.jsx
+++ src/Components/signUp.jsx
@@ -54,30 +54,30 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="bg-[#0D0e10] flex justify-center items-center min-h-screen ">
-      <div className="bg-[#0D0e10] p-8 rounded-lg w-full max-w-md">
-        <h2 className="text-white text-2xl font-semibold mb-6 text-center">Create a new account</h2>
+    <div className="bg-black flex justify-center items-center min-h-screen">
+      <div className="bg-black p-8 rounded-lg w-full max-w-md">
+        <h2 className="text-white text-2xl font-semibold mb-6 text-center">Sign Up</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
             placeholder="Name"
-            className="w-full px-2 py-2 rounded-md border-b border-gray-600 bg-[#0D0e10] text-white placeholder-gray-500 focus:outline-none focus:border-white"
+            className="w-full px-2 py-2 rounded-md border-b border-gray-600 bg-black text-white placeholder-gray-500 focus:outline-none focus:border-white"
           />
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
-            className="w-full px-2 py-2 rounded-md border-b border-gray-600 bg-[#0D0e10] text-white placeholder-gray-500 focus:outline-none focus:border-white"
+            className="w-full px-2 py-2 rounded-md border-b border-gray-600 bg-black text-white placeholder-gray-500 focus:outline-none focus:border-white"
           />
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
-            className="w-full px-2 py-2 rounded-md border-b border-gray-600 bg-[#0D0e10] text-white placeholder-gray-500 focus:outline-none focus:border-white"
+            className="w-full px-2 py-2 rounded-md border-b border-gray-600 bg-black text-white placeholder-gray-500 focus:outline-none focus:border-white"
           />
           <button type="submit" className="w-full py-2 bg-purple-700 text-white font-bold rounded-md hover:bg-purple-800 focus:outline-none">
             Continue
@@ -86,7 +86,7 @@ const SignUpPage = () => {
             <div className="text-red-600 text-center mt-2">{errorMessage}</div>
           )}
         </form>
-        <div className="text-12px text-white leading-tight sm:text-13px 2xl:text-sm mui-style-1okmvh8-termsText text-right">
+        <div className="text-xs text-white text-right mt-4">
           Already have an account?{" "}
           <Link to="/home/signin" className="text-blue-500 underline">
             Sign in here.
